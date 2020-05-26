6c3c2391e0049ab17b51f14af98fbf08f37a26a3
Richard Monesiglio <schiavo@monesiglio.net.br>
2020-05-26 07:38:24 -0300

    Adicionando validação de campos do formulário - correção no index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+
+?>
+<!DOVTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>formulário de inscrição</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device.width, initial.scale=1">
+</head>
+<body>
+<p> formulário para inscrição de com,petidores</p>
+<form action="script.php" method="post">
+    <p>Seu Nome: <input type="text" name="nome"></p>
+    <p>Sua idade: <input type="text" name="idade"></p>
+    <p><input type="submit" value="Enviar Dados do Competidor"></p>
+
+</form>
+</body>
+</html>
